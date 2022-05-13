6d6fb16a328903bf1980b1b43d20eab55171e0b9
Matthew Ross <mwrs@trustpilot.com>
2022-05-13 17:07:38 +0200

    Improve takeRandom for smaller datasets

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,23 +1,22 @@
 export function takeRandom(items: any[], max: number) {
-    
-    const selectedItems: any[] = [];
 
-    // If there are less items than the maximum, just use all the items.
-    if (items.length <= max) {
-        selectedItems.concat(items);
-    } else {
+    let internalMax: number = max;
+    let selectedItems: any[] = [];
 
-        while (selectedItems.length < max) {
+    // If there are fewer items than the max, the max should be the total number of items provided.
+    if (items.length < max) {
+        internalMax = items.length;
+    }
 
-            // Pick a random item from the array.
-            const selectedItem = items[Math.floor(Math.random() * items.length)];
+    while (selectedItems.length < internalMax) {
 
-            // If we haven't already selected it, select it.
-            // Otherwise, repeat the selection process.
-            if (!selectedItems.includes(selectedItem)) {
-                selectedItems.push(selectedItem);
-            }
+        // Pick a random item from the array.
+        const selectedItem = items[Math.floor(Math.random() * items.length)];
 
+        // If we haven't already selected it, select it.
+        // Otherwise, repeat the selection process.
+        if (!selectedItems.includes(selectedItem)) {
+            selectedItems.push(selectedItem);
         }
 
     }
